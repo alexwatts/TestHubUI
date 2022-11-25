668bf66950a5e64785408971368a51cc584a6897
alex <alex@alwa.com>
2022-11-25 19:59:55 +0000

    Fixed decofing

--- src/features/results/components/Job.tsx
+++ src/features/results/components/Job.tsx
@@ -3,12 +3,13 @@ import {JobProps} from "../types";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faLink} from "@fortawesome/free-solid-svg-icons";
 import Button from 'react-bootstrap/Button';
+import {decode} from 'html-entities';
 
 export function Job(props: JobProps) {
     return (
      <>
         { (props.properties && props.properties.length > 0 ) &&
-            <Button className='btn-sm' href={decodeURI(props.properties[0].value)} target='_blank'>
+            <Button className='btn-sm' href={decode(props.properties[0].value)} target='_blank'>
                 <FontAwesomeIcon icon={faLink}/>
             </Button>
         }
